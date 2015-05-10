26fda8eaa1e780ccc9df172ad59563e6dce1e9e3
Abinash Meher <abinashdakshana999@gmail.com>
2015-05-10 06:49:04 +0530

    Made src/ruby a minimal gem, renamed to symengine

--- src/ruby/csympy.gemspec
+++
@@ -1,9 +0,0 @@
-Gem::Specification.new do |gem|
-	gem.name = 'csympy'
-	gem.version = '0.0.0'
-	gem.summary = "Ruby interface for CSymPy, a fast C++ library for symbolic manipulation"
-	gem.description = "Ruby interface fro CSymPy, a fast C++ library for symbolic manipulation"
-	gem.authors = ['Abinash Meher']
-	gem.files = ["lib/csympy.rb"]
-	gem.license = 'MIT'
-end
---
+++ src/ruby/ext/symengine/extconf.rb
@@ -0,0 +1,22 @@
+require 'mkmf'
+
+#Gives the ability to easily use alternate compilers to build the extension
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+
+extension_name = 'symengine/symengine'
+
+#Check to see if the csympy library required to build this extension exists.
+#Typically, we would want to use libcsympy installed, including the header
+#files
+#unless have_library("libcsympy")
+#    raise "libcsympy not found"
+#end
+#How is libcsympy installed? Not able to find libcsympy
+
+#creates the header file extconf.h, based on the results from all of the
+#previous have_*() functions.
+#The extconf.h file will be included by all of the C files in the project
+#to gain access to the HAVE_* macros that extconf defines.
+create_header
+
+create_makefile(extension_name)
