fb6861ba3795c2c2d0d9a57954da107257e93b1a
Abinash Meher <abinashdakshana999@gmail.com>
2015-05-31 18:04:29 +0530

    Changes to build the ruby extensions in extconf.rb CMakelists and Rakefile

--- src/ruby/ext/symengine/extconf.rb
+++ src/ruby/ext/symengine/extconf.rb
@@ -1,20 +1,22 @@
 require 'mkmf'
 
 #To avoid the error "can not be used when making a shared object; recompile with -fPIC"
-$CFLAGS = ["-Wall -Werror=return-type -fPIC",$CFLAGS].join(" ")
+$CFLAGS << ' -Wall' unless $CFLAGS.split.include?('-Wall')
+$CFLAGS << ' -Werror' unless $CFLAGS.split.include?('-Werror')
+$CFLAGS << ' -fPIC' unless $CFLAGS.split.include?('-fpic')
 
 #Gives the ability to easily use alternate compilers to build the extension
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 extension_name = 'symengine/symengine'
 
-#Check to see if the csympy library required to build this extension exists.
-#Typically, we would want to use libcsympy installed, including the header
+#Check to see if the symengine library required to build this extension exists.
+#Typically, we would want to use libsymengine installed, including the header
 #files
-unless have_library("symengine")
-    raise "libsymengine not found"
-end
-#How is libcsympy installed? Not able to find libcsympy
+#unless pkg_config('symengine')
+#    raise "libsymengine not found"
+#end
+have_library('symengine')
 
 #creates the header file extconf.h, based on the results from all of the
 #previous have_*() functions.
