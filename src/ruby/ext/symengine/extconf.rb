93715f5b61849fc8b8bac568aa86e9ce15f855fd
Abinash Meher <abinashdakshana999@gmail.com>
2015-06-15 20:28:52 +0530

    Tried to use `g++` compiler

--- src/ruby/ext/symengine/extconf.rb
+++ src/ruby/ext/symengine/extconf.rb
@@ -37,8 +37,11 @@ $CFLAGS << ' -Werror' unless $CFLAGS.split.include?('-Werror')
 $CFLAGS << ' -fPIC' unless $CFLAGS.split.include?('-fpic')
 # $CFLAGS << ' -std=c99' unless $CFLAGS.split.include?('-std=c99')
 
+RbConfig::CONFIG['CC'] = 'g++'
+RbConfig::CONFIG['CXX'] = 'g++'
+$CPPFLAGS='-Wall -O3 -Wall -Wcast-qual -Wconversion -DSGI__gnu_cxx -DREADLINE'
 # Gives the ability to easily use alternate compilers to build the extension
-RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+#RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 # Giving it a name
 extension_name = 'symengine/symengine'
@@ -52,10 +55,15 @@ dir_config(extension_name)
 # unless pkg_config('symengine')
 #    raise "libsymengine not found"
 # end
-# unless have_library('symengine', 'basic_init')
-#     abort 'Unable to find basic_init in library symengine!'
-# end
-# $libs = append_library($libs, 'symengine')
+unless have_header('cwrapper.h')
+    raise 'cwrapper.h not found'
+end
+unless have_library('symengine',
+                    'basic p; basic_init(p); basic_free(p)',
+                    'cwrapper.h')
+    abort 'Unable to find basic_init in library symengine!'
+end
+$libs = append_library($libs, 'symengine')
 
 # creates the header file extconf.h, based on the results from all of the
 # previous have_*() functions.
